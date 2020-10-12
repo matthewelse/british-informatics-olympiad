5a7e358bc171738becccdcd0291552f604ed331d
Matthew Else <759697+matthewelse@users.noreply.github.com>
2020-10-12 21:42:08 +0100

    neater q3.py

--- 2019/q3.py
+++ 2019/q3.py
@@ -1,82 +1,78 @@
 """
-q3 2019 bio
+2019 Q3: Block-chain
 
-I'm pretty confident that this solution is correct, but it's too slow for the
-later examples. I wonder whether it'd be faster in C
+This solution is too slow for 3 of the harder examples in the mark scheme, but I'm pretty sure it's
+correct. The C++ solution is faster, fast enough for all but the hardest example.
+
+This solution uses depth-first search to enumerate the possible combinations, eliminating invalid
+additions by detecting triples of a, b, c where a < b < c as we try to add new blocks to the prefix.
 """
 
-l, prefix = input().split(' ')
 
-l = int(l)
+def is_valid_to_add(prefix, new_char, available):
+    """
+    We assume that prefix is already a valid block-chain, by induction. Returns whether or not prefix
+    + new_char is a valid block-chain.
 
-# dfs => stack
+    We can do this fast, in one iteration over prefix, by keeping track of the smallest character we've
+    seen so far, and then for each subsequent character, seeing whether it forms a triple with the new
+    character that would cause it to not be a block-chain.
+    """
 
-count = 0
-chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:l]
-prefix = [chars.find(c) for c in prefix]
-
-used = 0
-all = 0
-
-for i in range(l):
-    all |= 1 << i
-
-for i in prefix:
-    used |= 1 << i
-
-stack = [ (prefix, used) ]
-mask = 0xFFFFFFFF << l
-
-def is_valid(s, c, used):
-    # is it valid to add c to s?
     smallest = None
 
-    for ch in s:
-        if smallest is None or ch < smallest:
-            smallest = ch
-        elif ch > smallest and ch < c:
+    for c in prefix:
+        if smallest is None or c < smallest:
+            smallest = c
+        elif smallest < c and c < new_char:
+            # invalid block-chain
             return False
 
-    if smallest is not None and c > smallest:
-        base = 0x000FFFFF
+    """
+    Turns out that we can go one step further than this. Since we know that every other character in
+    available has to go after this new_char, we know that for any character in available, the new char
+    must be the largest (i.e. nothing can be larger than new_char)
+    
+    smallest < new_char < character in available
 
-        if (used & (base << (c + 1))) != (all & (base << (c + 1))):
-            print("invalid move: %s (adding %d) (smallest: %d)" % (s, c, smallest))
-            return False
-        #for i in range(1 + c, l):
-        #    if (used & (1 << i)) != 1 << i:
-        #        return False
+    > We could probably make this faster using a priority queue, but given that my highly optimised
+    version in C++ wasn't fast enough, I think I'm missing something more important.
+    """
+
+    if smallest < new_char:
+        for c in available:
+            if new_char < c:
+                return False
 
     return True
 
-def can_prune(s, used):
-    smallest = None
 
-    for ch in s:
-        if smallest is None or ch < smallest:
-            smallest = ch
-        elif ch > smallest:
-            # if there are any remaining chars larger than this, we can't do anything
-            # at all
-            if used & (0xFFFFFFFF << (ch + 1)) == 0 and ch != l - 1:
-                print("pruning %s (smallest: %d) (next : %d) (used : %x)" % (s, smallest, ch, used))
-                return True
+def count(prefix, available, target_length) -> int:
+    if len(prefix) == target_length:
+        # exactly one possible solution
+        return 1
 
-    return False
+    total = 0
 
-while len(stack) > 0:
-    #print(stack)
-    top, used = stack.pop()
-    print(top)
+    for c in list(available):
+        available.remove(c)
+        if is_valid_to_add(prefix, c, available):
+            prefix.append(c)
+            total += count(prefix, available, target_length)
+            prefix.pop()
+        available.add(c)
 
-    if used == all:
-        count += 1
-    elif not can_prune(top, used):
-        for c in range(l):
-            if used & (1 << c) == 0 and is_valid(top, c, used):
-                stack.append((top + [c], used | (1 << c)))
-    else:
-        print("pruned %s" % top)
+    return total
 
-print(count)
 
+length, prefix = input().split(" ")
+length = int(length)
+
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:length]
+
+available = set(chars) - set(prefix)
+prefix = list(prefix)
+
+result = count(prefix, available, length)
+
+print(result)
