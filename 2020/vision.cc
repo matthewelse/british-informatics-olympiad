a93d4294cb2f43f3cffbd9fa5ecab0d67d064cc5
Matthew Else <759697+matthewelse@users.noreply.github.com>
2020-10-18 20:41:04 +0100

    tidy

--- 2020/vision.cc
+++ 2020/vision.cc
@@ -1,15 +1,25 @@
+/**
+ * This problem seemed surprisingly easy, so maybe I missed a more efficient way
+ * to answer this problem than DFS? I threw in some memoisation in case that
+ * speeds things up, but there's no mark scheme so shrug.
+ */
+
 #include <iostream>
 #include <limits>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-int max_satisfaction(const vector<int> &telescopes, int position,
+int max_satisfaction(unordered_map<int, unordered_map<int, int>> memos,
+                     const vector<int> &telescopes, int position,
                      int step_count) {
   if (step_count == 0) {
     return 0;
   } else if (step_count == 1) {
     return telescopes[position];
+  } else if (memos[position].find(step_count) != memos[position].end()) {
+    return memos[position][step_count];
   }
 
   int satisfaction = telescopes[position];
@@ -18,11 +28,14 @@ int max_satisfaction(const vector<int> &telescopes, int position,
   for (int direction : {-1, 1}) {
     int next_position = direction + position;
     if (next_position >= 0 && next_position < telescopes.size()) {
-      int score = max_satisfaction(telescopes, next_position, step_count - 1);
+      int score =
+          max_satisfaction(memos, telescopes, next_position, step_count - 1);
 
       best = score > best ? score : best;
     }
   }
+
+  memos[position][step_count] = satisfaction + best;
 
   return satisfaction + best;
 }
@@ -44,9 +57,10 @@ int main() {
   }
 
   int total = 0;
+  unordered_map<int, unordered_map<int, int>> memos;
 
   for (auto telescope_count : walkers) {
-    total += max_satisfaction(telescopes, 0, telescope_count);
+    total += max_satisfaction(memos, telescopes, 0, telescope_count);
   }
 
   cout << total << endl;
