99bd71ad14ddf86c081d62e681400afe9a6e8e57
Matthew Else <759697+matthewelse@users.noreply.github.com>
2021-04-08 20:19:19 +0100

    2021 Q3

---
+++ 2021/q3.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solve(target):
+    target = tuple(target)
+    choices = deque([(0, tuple())])
+
+    while len(choices) > 0:
+        steps, display = choices.popleft()
+
+        if display == target:
+            return steps
+        
+        # add
+        if len(display) == 0:
+            next_one = 0
+        else:
+            next_one = max(display) + 1
+
+        choices.append((steps + 1, display + tuple([next_one])))
+    
+        # swap
+        if len(display) >= 2:
+            choices.append((steps + 1, display[1:2] + display[:1] + display[2:]))
+
+        # rotate
+        if len(display) >= 2:
+            choices.append((steps + 1,  display[1:] + display[:1]))
+        
+if __name__ == '__main__':
+    target = list(ord(x) - ord('A') for x in input())
+
+    warehouse = list(reversed(sorted(target)))
+
+    print(solve(target))
